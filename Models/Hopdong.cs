8d22475d11aadd2a913ef205ad466ddcb71d64f6
Nguyen Tai Duc <taiduc8098@gmail.com>
2023-05-29 12:16:59 +0700

    update29

--- Models/Hopdong.cs
+++ Models/Hopdong.cs
@@ -5,10 +5,13 @@ namespace QLNS.Models
     public class Hopdong
     { 
         [Key]
+        [Required(ErrorMessage ="Mã nhân viên không được bỏ trống")]
         [Display(Name ="Mã Nhân viên")]
         public string MaNV { get; set; }
+        [Required(ErrorMessage ="Không được bỏ trống ngày")]
         [Display(Name ="Ngày bắt đầu")]
         public string Ngaybatdau { get; set; }
+        [Required(ErrorMessage ="Không được bỏ trống ngày")]
         [Display(Name ="Ngày kết thúc")]
         public string Ngayketthuc { get; set; }
     }
